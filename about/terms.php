b370c2c311b5f68ce49ac3a0b545025a72b9e155
PinzhenChen <pinzhen_chen@yahoo.com>
2019-01-27 15:46:03 +0000

    color in about us

--- about/terms.php
+++ about/terms.php
@@ -7,55 +7,55 @@
 <main>
     <div class = "container">  
         <div class = "container-fliud"  id="blur">
-            <h1>Terms and Conditions ("Terms")</h1>
+            <h1 class="textOrange">Terms and Conditions ("Terms")</h1>
 
 
-<p>Last updated: January 27, 2019</p>
+<p class="textWhite textFontLarge">Last updated: January 27, 2019</p>
 
 
-<p>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the www.tradedin.co.uk website (the "Service") operated by EasyDRPS ("us", "we", or "our").</p>
+<p class="textWhite textFontLarge">Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the www.tradedin.co.uk website (the "Service") operated by EasyDRPS ("us", "we", or "our").</p>
 
-<p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+<p class="textWhite textFontLarge">Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
 
-<p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. The Terms and Conditions agreement  for EasyDRPS has been created with the help of <a href="https://termsfeed.com/">TermsFeed</a>.</p>
-
-
-
-
-<h2>Links To Other Web Sites</h2>
-
-<p>Our Service may contain links to third-party web sites or services that are not owned or controlled by EasyDRPS.</p>
-
-<p>EasyDRPS has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that EasyDRPS shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
-
-<p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
-
-
-<h2>Termination</h2>
-
-<p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
-
-<p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+<p class="textWhite textFontLarge">By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. The Terms and Conditions agreement  for EasyDRPS has been created with the help of <a href="https://termsfeed.com/">TermsFeed</a>.</p>
 
 
 
-<h2>Governing Law</h2>
 
-<p>These Terms shall be governed and construed in accordance with the laws of United Kingdom, without regard to its conflict of law provisions.</p>
+<h2 class="textOrange">Links To Other Web Sites</h2>
 
-<p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+<p class="textWhite textFontLarge">Our Service may contain links to third-party web sites or services that are not owned or controlled by EasyDRPS.</p>
 
+<p class="textWhite textFontLarge">EasyDRPS has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that EasyDRPS shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
 
-<h2>Changes</h2>
-
-<p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 15 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
-
-<p>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+<p class="textWhite textFontLarge">We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
 
 
-<h2>Contact Us</h2>
+<h2 class="textOrange">Termination</h2>
 
-<p>If you have any questions about these Terms, please contact us at s1527244@sms.ed.ac.uk or s1533564@sms.ed.ac.uk.</p>
+<p class="textWhite textFontLarge">We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+
+<p class="textWhite textFontLarge">All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+
+
+
+<h class="textOrange">Governing Law</h2>
+
+<p class="textWhite textFontLarge">These Terms shall be governed and construed in accordance with the laws of United Kingdom, without regard to its conflict of law provisions.</p>
+
+<p class="textWhite textFontLarge">Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+
+
+<h2 class="textOrange">Changes</h2>
+
+<p class="textWhite textFontLarge">We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 15 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+
+<p class="textWhite textFontLarge">By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+
+
+<h2 class="textOrange">Contact Us</h2>
+
+<p class="textWhite textFontLarge">If you have any questions about these Terms, please contact us at s1527244@sms.ed.ac.uk or s1533564@sms.ed.ac.uk.</p>
         </div>                         
     </div>
 </main>
