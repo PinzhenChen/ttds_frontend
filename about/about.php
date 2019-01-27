55fdbfc5b41bf835cda65b2476f4fee807beab8a
PinzhenChen <pinzhen_chen@yahoo.com>
2019-01-27 15:49:11 +0000

    wording

--- about/about.php
+++ about/about.php
@@ -9,16 +9,16 @@
 		<div class = "container-fliud"  id="blur">
 			<h2 class="textWhite" style="text-align: center;">About Us</h2>
 			<h3 class="textOrange">Who We Are</h3>
-			<p class="textWhite textFontLarge">We are a coursework group for TTDS 2018-19<br/><p>
+			<p class="textWhite textFontLarge">We are a coursework group for TTDS 2018-19.<br/><p>
 			<h3 class="textOrange">Our Mission</h3>
-			<p class="textWhite textFontLarge">We are devoted to better DRPS information retrieval<br/></p>
+			<p class="textWhite textFontLarge">We are devoted to providing better DRPS information retrieval.<br/></p>
 			<h3 class="textOrange">How To Use</h3>
-			<p class="textWhite textFontLarge">It is a search engine with AI functionality</p>
+			<p class="textWhite textFontLarge">It is a search engine with AI functionality. Try it out!</p>
 
-			<h3 class="textOrange">Contact Us</h3>
+			<h3 class="textOrange">Contact Us!</h3>
 			<div class="row">
 				<div class="col-md-2">
-					<h4 class="textWhite" style="color:hotpink;">&nbsp;Developers</h4>
+					<h4 class="textWhite" style="color:hotpink;">&nbsp;Web&nbsp;Developers</h4>
 				</div>
 				<div class="col-md-10">
 					<h5 class="textWhite textFontLarge">Zheng Zhao - <a href="mailto:zheng.zhao@ttds.co.uk" class="textWhite">s1533564@sms.ed.ac.uk</a></h5>
