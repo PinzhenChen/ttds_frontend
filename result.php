9828eac595a8ef110ff1f309c81d54c06a1bc930
Patrick Chen <s1527244@velletri.inf.ed.ac.uk>
2019-01-31 19:17:16 +0000

    test

--- result.php
+++ result.php
@@ -57,7 +57,7 @@ $course_list = $json['response']['docs'];
         <h4 class="textWhite">There are <?php echo $numFound;?> courses matching your search in <?php echo $option;?></h4>
         <div class = "container-fliud">
         <div id='blur'>
-          <h4 class="textWhite">Getting answer from our dedicated AI bot. Please Wait....</h4>
+          <h4 class="textWhite">Getting information from our dedicated AI bot. Please Wait....</h4>
           <img src="static/img/ajax-loader.gif" alt="Loading" />
         </div>
         <?php
