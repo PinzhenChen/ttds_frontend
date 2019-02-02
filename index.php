45aa2778a17c11a1722a06d60ead1a1f81990762
zsquaredz <zhaozheng1997@hotmail.com>
2019-02-02 17:11:20 +0000

    test

--- index.php
+++ index.php
@@ -39,15 +39,7 @@ if ($filter_flag == "1") {
 <<<HTML
                             <!-- SELECT BOX -->
                             <div class="form-group">
-                              <label for="sel1">Select list (select one):</label>
-                              <select class="form-control" id="sel1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                              </select>
-                              <br>
-                              <label for="sel2">Mutiple select list (hold shift to select more than one):</label>
+                              <label for="sel2">School (select multiple items by holding shift)</label>
                               <select multiple class="form-control" id="sel2">
                                 <option>1</option>
                                 <option>2</option>
